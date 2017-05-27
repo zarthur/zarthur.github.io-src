7ecd80f626034a767755658583690483e1fd1cd0
Arthur Neuman <arthur.neuman@gmail.com>
2017-05-27 13:32:33 -0400

    change theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,17 +19,21 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-# LINKS = (('Pelican', 'http://getpelican.com/'),
-#          ('Python.org', 'http://python.org/'),
-#          ('Jinja2', 'http://jinja.pocoo.org/'),
-#          ('You can moGitdify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('LinkedIn', 'https://www.linkedin.com/in/arthurneuman/'),
-          ('GitHub', 'https://github.com/zarthur'),)
-
 DEFAULT_PAGINATION = 10
+
+STATIC_PATHS = ['extras', 'images']
+EXTRA_PATH_METADATA = {
+    'extras/android-chrome-192x192.png': {'path': 'android-chrome-192x192.png'},
+    'extras/android-chrome-512x512.png': {'path': 'android-chrome-512x512.png'},
+    'extras/apple-touch-icon.png': {'path': 'apple-touch-icon.png'},
+    'extras/browserconfig.xml': {'path': 'browserconfig.xml'},
+    'extras/favicon-16x16.png': {'path': 'favicon-16x16.png'},
+    'extras/favicon-32x32.png': {'path': 'favicon-32x32.png'},
+    'extras/favicon.ico': {'path': 'favicon.ico'},
+    'extras/manifest.json': {'path': 'manifest.json'},
+    'extras/mstile-150x150.png': {'path': 'mstile-150x150.png'},
+    'extras/safari-pinned-tab.svg': {'path': 'safari-pinned-tab.svg'},
+}
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
@@ -38,4 +42,32 @@ MARKUP = ('md', 'ipynb')
 
 PLUGIN_PATHS = ['./plugins']
 PLUGINS = ['ipynb.markup']
+
+# Theme Settings
+
+THEME = 'themes/pelican-alchemy/alchemy'
+
+# Blogroll
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can moGitdify those links in your config file', '#'),)
+
+
+# Social widget
+# SOCIAL = (('LinkedIn', 'https://www.linkedin.com/in/arthurneuman/'),
+#           ('GitHub', 'https://github.com/zarthur'),)
+
+SITESUBTITLE = "Data, development, and other topics"
+
+SITEIMAGE = "/images/image.png width=200 height=200"
+
+ICONS = (
+    ('linkedin-square', 'https://www.linkedin.com/in/arthurneuman/'),
+    ('github', 'https://github.com/zarthur'),
+)
+
+HIDE_AUTHORS = True
+
+RFG_FAVICONS = True
 
